67b292df234d130b5b2078b049dfd24af9ae470d
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-12-05 11:44:26 -0300

    chore: spring boot 4.0 init

--- backend/src/main/java/br/com/riannegreiros/backend/BackendApplication.java
+++ backend/src/main/java/br/com/riannegreiros/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package xyz.riannegreiros.spring_nextjs_start_kit;
+package br.com.riannegreiros.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringNextjsStartKitApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringNextjsStartKitApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
